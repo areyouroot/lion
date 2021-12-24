651ba73ce220ac480aefca09234c4b2ac4276720
abdul_faheem <abdulfaheemasd@gmail.com>
2021-12-24 10:33:50 +0530

    django web server is added

--- pre-req.sh
+++ pre-req.sh
@@ -2,5 +2,6 @@ echo "download chrome and press any key to continuew"
 read a
 sudo apt install git python3 python3-pip
 pip install django
-
+echo "download vscode and press any key to continuew"
+read a
 
